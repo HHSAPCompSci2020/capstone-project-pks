99b9b6c18ec35b637ef99a0d9ecff3fe39a8a148
kyu989 <kyu989@users.noreply.github.com>
2021-05-20 10:56:54 -0700

    Drawings

---
+++ HighSchoolJourney/src/DrawingSurface.java
@@ -0,0 +1,60 @@
+
+import java.awt.event.KeyEvent;
+import processing.core.PApplet;
+
+public class DrawingSurface extends PApplet {
+
+	// FIELDS
+
+
+	// CONSTRUCTOR - Initialize any added fields here.
+	public DrawingSurface() {
+
+	}
+
+
+	// METHODS
+	// Add processing methods here. See the documentation at processing.org for reference.
+	public void setup() {
+
+	}
+
+	public void draw() {
+		background(255);
+		pushStyle();
+		fill(0);
+		textSize(10);
+		popStyle();
+	}
+	
+	public void mousePressed() {
+	//	h.move(mouseX, mouseY);
+	}
+	
+	public void keyPressed()
+	{
+/*		if (keyCode == KeyEvent.VK_UP)
+		{
+			h.changeScale(2f);
+		}
+		if (keyCode == KeyEvent.VK_DOWN)
+		{
+			h.changeScale(0.5f);
+		}
+		if (keyCode == KeyEvent.VK_LEFT)
+		{
+			p.changeSide(-25f);
+		}
+		if (keyCode == KeyEvent.VK_RIGHT)
+		{
+			p.changeSide(25f);
+		}*/
+	}
+
+	// Add methods for user interaction. Check the processing reference for info on this (http://processing.org).
+	// We'll also review how to get mouse/keys working in class.
+
+
+
+
+}
