abdecf3b9980cd7dfc65f9c9368bbbf7a5377db6
SachinThapa04 <sthapa339@student.fuhsd.org>
2021-05-25 23:00:15 -0700

    Added Level Classes

---
+++ HighSchoolJourney/src/pks/highSchoolJourney/LevelFreshman.java
@@ -0,0 +1,18 @@
+package pks.highSchoolJourney;
+
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import processing.core.PApplet;
+
+public class LevelFreshman extends PApplet {
+	
+	private ArrayList<Shape> freshmanLevel;
+	
+	public LevelFreshman() {
+		freshmanLevel = new ArrayList<Shape>(); 
+		
+		freshmanLevel.add(new Rectangle(0,0,0,0));
+	}
+}
---
+++ HighSchoolJourney/src/pks/highSchoolJourney/LevelSophmore.java
@@ -0,0 +1,20 @@
+package pks.highSchoolJourney;
+
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import processing.core.PApplet;
+
+public class LevelSophmore extends PApplet {
+
+	private ArrayList<Shape> sophmoreLevel;
+	
+	public LevelSophmore() {
+		
+		sophmoreLevel = new ArrayList<Shape>();
+		
+		
+	}
+	
+}
